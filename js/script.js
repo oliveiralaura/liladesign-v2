2dca9223463dc4ef2b75ef3ea7c3aec1fef7855d
Laura <laura.pimenta2110@hotmail.com>
2024-04-28 17:32:52 -0300

    mudança

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         var scrollPos = $(window).scrollTop();
+//         if (scrollPos > 700) {
+//             $('.container-fluid').css('background-image', 'url("../images/L-branco.png")');
+//         } else {
+//             $('.container-fluid').css('background-image', 'url("../images/L.png")');
+//         }
+//     });
+// });
